d67ff4bd79583a03b50530da78a2fffb6014e756
scrampist <legor9325@gmail.com>
2022-05-29 19:24:17 +0300

    TdxtView

--- app/src/main/java/com/example/stoga/AcuActivity.java
+++ app/src/main/java/com/example/stoga/AcuActivity.java
@@ -26,6 +26,7 @@ public class AcuActivity extends AppCompatActivity {
     }
 
 
+
     public void read(View view) {
         read.setOnClickListener(new View.OnClickListener() {
             @Override
